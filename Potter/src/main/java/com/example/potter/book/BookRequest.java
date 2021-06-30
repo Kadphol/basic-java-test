94dea396f700f35695f485e4e1df5d2a399940ea
Kadphol <kadphol.len@outlook.co.th>
2021-06-30 16:04:53 +0700

    [ADD] BookRequest class for Order

--- Potter/src/main/java/com/example/potter/book/BookRequest.java
+++ Potter/src/main/java/com/example/potter/book/BookRequest.java
@@ -1,2 +1,21 @@
-package com.example.potter.book;public class BookRequest {
+package com.example.potter.book;
+
+public class BookRequest {
+    private int id;
+    private int count;
+
+    public BookRequest() { }
+
+    public BookRequest(int id, int count) {
+        this.id = id;
+        this.count = count;
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public int getCount() { return count; }
+
+    public void setCount(int count) { this.count = count; }
 }
