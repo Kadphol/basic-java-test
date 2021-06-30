cb46a6b7a100c2ad92164794f9e6a41f055d2540
Kadphol <kadphol.len@outlook.co.th>
2021-06-30 12:44:15 +0700

    [ADD] initial Spring Boot project

---
+++ Potter/src/main/java/com/example/potter/PotterApplication.java
@@ -0,0 +1,13 @@
+package com.example.potter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotterApplication.class, args);
+    }
+
+}
