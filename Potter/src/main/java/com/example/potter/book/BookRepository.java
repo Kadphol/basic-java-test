fcf2fdf6adb40cc51b1f1279d696889843c6afe3
Kadphol <kadphol.len@outlook.co.th>
2021-06-30 14:48:23 +0700

    [FIX] change extends method to JpaRepository

--- Potter/src/main/java/com/example/potter/book/BookRepository.java
+++ Potter/src/main/java/com/example/potter/book/BookRepository.java
@@ -1,8 +1,8 @@
 package com.example.potter.book;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Integer> {
+public interface BookRepository extends JpaRepository<Book, Integer> {
 }
