1b0977746816887852afbe80c00b9f7d6f5808c7
Kadphol <kadphol.len@outlook.co.th>
2021-06-30 17:08:36 +0700

    [ADD] Test for Basic Discount

--- Potter/src/test/java/com/example/potter/book/BookServiceTest.java
+++ Potter/src/test/java/com/example/potter/book/BookServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -44,5 +45,33 @@ class BookServiceTest {
 
         BookNotFoundException exception = assertThrows(BookNotFoundException.class, () -> service.getBook(100));
         assertEquals("Book id 100 NOT FOUND!",exception.getMessage());
+    }
+
+    @Test
+    public void discountTest() {
+        BookService service = new BookService();
+        assertEquals(0, service.discount(0));
+        assertEquals(100, service.discount(1));
+        assertEquals(190, service.discount(2));
+        assertEquals(270, service.discount(3));
+        assertEquals(320, service.discount(4));
+        assertEquals(375, service.discount(5));
+    }
+
+    @Test
+    public void testBasicPrice() {
+        BookService service = new BookService();
+        assertEquals(100, service.calculatePrice(Arrays.asList(1)));
+        assertEquals(100*2, service.calculatePrice(Arrays.asList(1,1)));
+        assertEquals(100*3, service.calculatePrice(Arrays.asList(1,1,1)));
+    }
+
+    @Test
+    public void testBasicDiscountPrice() {
+        BookService service = new BookService();
+        assertEquals(100*2*0.95, service.calculatePrice(Arrays.asList(1,2)));
+        assertEquals(100*3*0.9, service.calculatePrice(Arrays.asList(1,2,3)));
+        assertEquals(100*4*0.8, service.calculatePrice(Arrays.asList(1,2,3,4)));
+        assertEquals(100*5*0.75, service.calculatePrice(Arrays.asList(1,2,3,4,5)));
     }
 }
